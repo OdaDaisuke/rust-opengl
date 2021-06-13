54a926aae4988d1aafad4d561af50e441d01a186
OdaDaisuke <daisukeoda3141@gmail.com>
2021-06-13 18:31:49 +0900

    texture

--- src/opengl.rs
+++ src/opengl.rs
@@ -1,5 +1,6 @@
 use std::mem;
 use std::os::raw::c_void;
+use std::path::Path;
 use std::time::Duration;
 
 use c_str_macro::c_str;
@@ -10,9 +11,11 @@ use imgui::im_str;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
+mod image_manager;
 mod shader;
 mod vertex;
 
+use image_manager::ImageManager;
 use shader::Shader;
 use vertex::Vertex;
 
@@ -23,9 +26,9 @@ type Vector3 = cgmath::Vector3<f32>;
 #[allow(dead_code)]
 type Matrix4 = cgmath::Matrix4<f32>;
 
-const WINDOW_WIDTH: u32 = 640;
+const WINDOW_WIDTH: u32 = 900;
 const WINDOW_HEIGHT: u32 = 480;
-const FLOAT_NUM: usize = 3;
+const FLOAT_NUM: usize = 8;
 const VERTEX_NUM: usize = 36;
 const BUF_LEN: usize = FLOAT_NUM * VERTEX_NUM;
 
@@ -53,72 +56,75 @@ pub fn run () {
     
     let _gl_context = window.gl_create_context().unwrap();
     gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as _);
+    let mut image_manager = ImageManager::new();
+    image_manager.load_image(Path::new("./rsc/image.png"), "surface", true);
+
     let shader = Shader::new("./rsc/shader/shader.vs", "./rsc/shader/shader.fs");
 
     // set buffer
     #[rustfmt::skip]
     let buffer_array: [f32; BUF_LEN] = [
         // 1
-        ZERO, ZERO, ZERO,
-        ZERO, ONE, ZERO,
-        ONE, ONE, ZERO,
+        0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0,
+        1.0, 1.0, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0,
 
-        ZERO, ZERO, ZERO,
-        ONE, ONE, ZERO,
-        ONE, ZERO, ZERO,
+        0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0,
+        1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 1.0,
 
         // 2
-        ZERO, ZERO, ONE,
-        ZERO, ZERO, ZERO,
-        ONE, ZERO, ZERO,
+        0.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 0.0,
+        1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0,
 
-        ZERO, ZERO, ONE,
-        ONE, ZERO, ZERO,
-        ONE, ZERO, ONE,
+        0.0, 0.0, 1.0, 0.0, -1.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0,
+        1.0, 0.0, 1.0, 0.0, -1.0, 0.0, 1.0, 1.0,
 
         // 3
-        ZERO, ONE, ONE,
-        ZERO, ZERO, ONE,
-        ONE, ZERO, ONE,
+        0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+        1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0,
 
-        ZERO, ONE, ONE,
-        ONE, ZERO, ONE,
-        ONE, ONE, ONE,
+        0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+        1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
 
         // 4
-        ZERO, ONE, ZERO,
-        ZERO, ONE, ONE,
-        ONE, ONE, ONE,
+        0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+        1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
 
-        ZERO, ONE, ZERO,
-        ONE, ONE, ONE,
-        ONE, ONE, ZERO,
+        0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+        1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
 
         // 5
-        ONE, ZERO, ONE,
-        ONE, ZERO, ZERO,
-        ONE, ONE, ZERO,
+        1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
 
-        ONE, ZERO, ONE,
-        ONE, ONE, ZERO,
-        ONE, ONE, ONE,
+        1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+        1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
+        1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0,
 
         // 6
-        ZERO, ONE, ONE,
-        ZERO, ONE, ZERO,
-        ZERO, ZERO, ZERO,
+        0.0, 1.0, 1.0, -1.0, 0.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,
 
-        ZERO, ONE, ONE,
-        ZERO, ZERO, ZERO,
-        ZERO, ZERO, ONE
+        0.0, 1.0, 1.0, -1.0, 0.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, -1.0, 0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
     ];
 
     let vertex = Vertex::new(
         (BUF_LEN * mem::size_of::<GLfloat>()) as GLsizeiptr,
         buffer_array.as_ptr() as *const c_void,
         gl::STATIC_DRAW,
-        vec![gl::FLOAT],
-        vec![FLOAT_NUM as i32],
+        vec![gl::FLOAT, gl::FLOAT, gl::FLOAT],
+        vec![3, 3, 2],
         FLOAT_NUM as i32 * mem::size_of::<GLfloat>() as GLsizei,
         VERTEX_NUM as i32,
     );
@@ -137,9 +143,37 @@ pub fn run () {
     let mut blend: bool = true;
     let mut wireframe: bool = true;
     let mut culling: bool = true;
-    let mut camera_x: f32 = 5.0f32;
-    let mut camera_y: f32 = -5.0f32;
-    let mut camera_z: f32 = 5.0f32;
+    let mut camera_x: f32 = 2.0f32;
+    let mut camera_y: f32 = -2.0f32;
+    let mut camera_z: f32 = 2.0f32;
+    let mut alpha: f32 = 1.0f32;
+    let mut material_specular: Vector3 = Vector3 {
+        x: 0.2,
+        y: 0.2,
+        z: 0.2,
+    };
+    let mut material_shininess: f32 = 0.1f32;
+    let mut light_direction: Vector3 = Vector3 {
+        x: 1.0,
+        y: 1.0,
+        z: 0.0,
+    };
+    let mut ambient: Vector3 = Vector3 {
+        x: 0.3,
+        y: 0.3,
+        z: 0.3,
+    };
+    let mut diffuse: Vector3 = Vector3 {
+        x: 0.5,
+        y: 0.5,
+        z: 0.5,
+    };
+    let mut specular: Vector3 = Vector3 {
+        x: 0.2,
+        y: 0.2,
+        z: 0.2,
+    };
+    let surface_texture_id = image_manager.get_texture_id("surface");
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
@@ -216,8 +250,18 @@ pub fn run () {
             shader.set_mat4(c_str!("uModel"), &model_matrix);
             shader.set_mat4(c_str!("uView"), &view_matrix);
             shader.set_mat4(c_str!("uProjection"), &projection_matrix);
+            shader.set_float(c_str!("uAlpha"), alpha);
+            shader.set_vec3(c_str!("uViewPosition"), camera_x, camera_y, camera_z);
+            shader.set_vector3(c_str!("uMaterial.specular"), &material_specular);
+            shader.set_float(c_str!("uMaterial.shininess"), material_shininess);
+            shader.set_vector3(c_str!("uLight.direction"), &light_direction);
+            shader.set_vector3(c_str!("uLight.ambient"), &ambient);
+            shader.set_vector3(c_str!("uLight.diffuse"), &diffuse);
+            shader.set_vector3(c_str!("uLight.specular"), &specular);
 
+            gl::BindTexture(gl::TEXTURE_2D, surface_texture_id as u32);
             vertex.draw();
+            gl::BindTexture(gl::TEXTURE_2D, 0);
             imgui_sdl2_context.prepare_frame(
                 imgui_context.io_mut(),
                 &window,
@@ -257,7 +301,81 @@ pub fn run () {
                     #[rustfmt::skip]
                     imgui::Slider::new(im_str!("Camera Z"))
                         .range(-5.0..=5.0)
-                        .build(&ui, &mut camera_z);
+                        .build(&ui, &mut camera_z);                    
+                });
+                imgui::Window::new(im_str!("Light"))
+                .size([300.0, 450.0], imgui::Condition::FirstUseEver)
+                .position([600.0, 10.0], imgui::Condition::FirstUseEver)
+                .build(&ui, || {
+                    imgui::Slider::new(im_str!("Alpha"))
+                        .range(0.0..=1.0).build(&ui, &mut alpha);
+
+                    ui.separator();
+
+                    imgui::Slider::new(im_str!("Material Specular X"))
+                        .range(0.0..=1.0).build(&ui, &mut material_specular.x);
+                    imgui::Slider::new(im_str!("Material Specular Y"))
+                        .range(0.0..=1.0).build(&ui, &mut material_specular.y);
+                    imgui::Slider::new(im_str!("Material Specular Z"))
+                        .range(0.0..=1.0).build(&ui, &mut material_specular.z);
+
+                    imgui::Slider::new(im_str!("Material Shininess"))
+                        .range(0.0..=2.0)
+                        .build(&ui, &mut material_shininess);
+
+                    ui.separator();
+
+                    imgui::Slider::new(im_str!("Direction X"))
+                        .range(-1.0..=1.0)
+                        .build(&ui, &mut light_direction.x);
+                    imgui::Slider::new(im_str!("Direction Y"))
+                        .range(-1.0..=1.0)
+                        .build(&ui, &mut light_direction.y);
+                    imgui::Slider::new(im_str!("Direction Z"))
+                        .range(-1.0..=1.0)
+                        .build(&ui, &mut light_direction.z);
+
+                    ui.separator();
+
+                    #[rustfmt::skip]
+                    imgui::Slider::new(im_str!("Ambient R"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut ambient.x);
+                    #[rustfmt::skip]
+                    imgui::Slider::new(im_str!("Ambient G"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut ambient.y);
+                    #[rustfmt::skip]
+                    imgui::Slider::new(im_str!("Ambient B"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut ambient.z);
+
+                    ui.separator();
+
+                    #[rustfmt::skip]
+                    imgui::Slider::new(im_str!("Diffuse R"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut diffuse.x);
+                    #[rustfmt::skip]
+                    imgui::Slider::new(im_str!("Diffuse G"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut diffuse.y);
+                    #[rustfmt::skip]
+                    imgui::Slider::new(im_str!("Diffuse B"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut diffuse.z);
+
+                    ui.separator();
+
+                    imgui::Slider::new(im_str!("Specular R"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut specular.x);
+                    imgui::Slider::new(im_str!("Specular G"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut specular.y);
+                    imgui::Slider::new(im_str!("Specular B"))
+                        .range(0.0..=1.0)
+                        .build(&ui, &mut specular.z);
                 });
             imgui_sdl2_context.prepare_render(&ui, &window);
             renderer.render(ui);
